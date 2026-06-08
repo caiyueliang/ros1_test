d56a4a15ed478daf46d300497466adac9ec7c5b9
yangsheng <690963696@qq.com>
2026-06-08 17:44:19 +0800

    update

--- catkin_ws/src/ssr_pkg/src/chao_node.cpp
+++ catkin_ws/src/ssr_pkg/src/chao_node.cpp
@@ -1,15 +1,26 @@
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 
 int main(int argc, char *argv[])
 {    
     ros::init(argc, argv, "chao_node");
+    // 创建ROS节点句柄，用于与ROS系统通信，管理节点资源
     ros::NodeHandle nh;
     printf("ros init success !\n");
+
+    // 创建发布者，用于发布消息，参数为: nh.advertise<std_msgs::消息类型>(话题名称, 缓存队列大小)
+    ros::Publisher pub = nh.advertise<std_msgs::String>("chao_topic", 10);
     
+    ros::Rate loop_rate(1.0); // 1Hz
+
     while(ros::ok())
     {    
+        std_msgs::String msg;
+        msg.data = "chao: hello world";
+        pub.publish(msg);
+        
         printf("sleep 1s ...\n");
-        ros::Duration(1.0).sleep(); // 暂停1秒
+        loop_rate.sleep(); // 暂停1秒
     }
     return 0;
 }
