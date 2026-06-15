52cc002b7a4072967d1fc8d3411d3be32786e788
yangsheng <690963696@qq.com>
2026-06-15 17:57:17 +0800

    update

---
+++ catkin_ws/src/vel_pkg/src/vel_node.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "vel_node");
+    ros::NodeHandle nh;
+
+    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    ros::Rate loop_rate(10);
+    geometry_msgs::Twist vel_msg;
+    while (ros::ok())
+    {
+        vel_msg.linear.x = 0.5;
+        vel_msg.linear.y = 0.0;
+        vel_msg.linear.z = 0.0;
+        
+        vel_msg.angular.x = 0.0;
+        vel_msg.angular.y = 0.0;
+        vel_msg.angular.z = 0.5;
+
+        vel_pub.publish(vel_msg);
+        ros::spinOnce();
+        loop_rate.sleep(); 
+    }
+    return 0;
+}
