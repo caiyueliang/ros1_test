ea227b2fc258bb401156f8c8e1566dc509cfa505
yangsheng <690963696@qq.com>
2026-06-15 15:22:54 +0800

    update

---
+++ catkin_py/src/py_atr_pkg/scripts/ma_node.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+import rospy
+from std_msgs.msg import String
+
+if __name__ == '__main__':
+    rospy.init_node('ma_topic_py')
+    rospy.logwarn('[ma_topic_py] init_node success !')
+
+    pub = rospy.Publisher('ma_topic_py', String, queue_size=10)
+    rospy.logwarn('[ma_topic_py] publisher success !')
+
+    rate = rospy.Rate(1)
+
+    while not rospy.is_shutdown():
+        msg = String()
+        msg.data = '[ma_topic_py] hello world'
+        pub.publish(msg)
+        rospy.logwarn('[chao_topic_py] publish msg success !')
+        rate.sleep()
