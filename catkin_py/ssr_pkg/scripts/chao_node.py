80f86846a2b72ea9e67ae006d370a372ef0042cc
yangsheng <690963696@qq.com>
2026-06-15 14:18:15 +0800

    update

--- catkin_py/ssr_pkg/scripts/chao_node.py
+++ catkin_py/ssr_pkg/scripts/chao_node.py
@@ -6,4 +6,16 @@ from std_msgs.msg import String
 
 if __name__ == '__main__':
     rospy.init_node('chao_node')
-    rospy.logwarn('hello world')
+    rospy.logwarn('chao_node init_node success !')
+
+    pub = rospy.Publisher('chao_topic', String, queue_size=10)
+    rospy.logwarn('chao_node publisher success !')
+
+    rate = rospy.Rate(1)
+
+    while not rospy.is_shutdown():
+        msg = String()
+        msg.data = '[chao_node] hello world'
+        pub.publish(msg)
+        rospy.logwarn('chao_node publish msg success !')
+        rate.sleep()
