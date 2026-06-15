12ade8d3d7d2c97e68700f23cb5f12e4485a6c0d
yangsheng <690963696@qq.com>
2026-06-15 14:15:27 +0800

    updte

---
+++ catkin_py/ssr_pkg/scripts/chao_node.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+import rospy
+from std_msgs.msg import String
+
+if __name__ == '__main__':
+    rospy.init_node('chao_node')
+    rospy.logwarn('hello world')
