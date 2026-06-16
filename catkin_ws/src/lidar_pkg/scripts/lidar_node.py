01563fbb74463ece70b96acc864fe42169e6b1f9
yangsheng <690963696@qq.com>
2026-06-16 14:00:54 +0800

    update

--- catkin_ws/src/lidar_pkg/scripts/lidar_node.py
+++ catkin_ws/src/lidar_pkg/scripts/lidar_node.py
@@ -3,23 +3,35 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+count = 0
 
 def lidar_callback(msg):
+    global vel_cmd
+    global count
+      
     distance = msg.ranges[180]  # 前方距离
     rospy.loginfo(f"前方距离: {distance} 米")
-    if distance < 1.0:
+    
+    if count > 0:
+        count -= 1
+        return
+
+    vel_cmd = Twist()
+    if distance < 1.5:
         # 前方有障碍物
         rospy.logwarn("前方有障碍物")
+        vel_cmd.linear.x = 0.3
+        vel_cmd.angular.z = 0.5
+        count = 30
     else:
         # 前方没有障碍物
         rospy.loginfo("前方没有障碍物")
-    # rospy.loginfo(msg.ranges)
-    # rospy.loginfo(msg.angle_min)
-    # rospy.loginfo(msg.angle_max)
-    # rospy.loginfo(msg.angle_increment)
-    # rospy.loginfo(msg.range_min)
-    # rospy.loginfo(msg.range_max)
-    # rospy.loginfo(msg.scan_time)
+        vel_cmd.linear.x = 0.3
+        vel_cmd.angular.z = 0.0
+
+    vel_pub.publish(vel_cmd)
 
 
 if __name__ == '__main__':
@@ -27,5 +39,7 @@ if __name__ == '__main__':
     rospy.logwarn('[lidar_node_py] init_node success !')
 
     lidar_sub = rospy.Subscriber('/scan', LaserScan, lidar_callback, queue_size=10)
+    vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rospy.logwarn('[lidar_node_py] publisher success !')
 
     rospy.spin()
