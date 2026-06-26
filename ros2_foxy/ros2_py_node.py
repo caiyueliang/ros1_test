f3d01d4ecfeef279e59487af66fb7ea7980c32d1
yangsheng <690963696@qq.com>
2026-06-26 16:52:20 +0800

    update

--- ros2_foxy/ros2_py_node.py
+++ ros2_foxy/ros2_py_node.py
@@ -1,12 +1,15 @@
 import rclpy
 from rclpy.node import Node
 
-if __name__ == '__main__':
+def main():
     rclpy.init()
     node = Node('ros2_py_node')
-    node.get_logger().info('Hello from ROS 2 Python Node!')
+    node.get_logger().warn('Hello from ROS 2 Python Node!')
     rclpy.spin(node)
 
     node.get_logger().warn('停止，清理 Node')
     node.destroy_node()
     rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
